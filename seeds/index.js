ee8bc84f481b2a476a8f8ed812042f9fa45fedaf
mlthada7 <m.multahada@gmail.com>
2022-09-21 23:01:13 +0700

    campground index

--- seeds/index.js
+++ seeds/index.js
@@ -17,6 +17,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
+		// get a random index for cities
 		const random1000 = Math.floor(Math.random() * 1000);
 		const camp = new Campground({
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
