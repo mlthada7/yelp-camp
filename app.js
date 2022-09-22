a146a7ac0e6dcf4bfc444f796d2042cf448e74b4
mlthada7 <m.multahada@gmail.com>
2022-09-22 08:58:00 +0700

    campground new & create

--- app.js
+++ app.js
@@ -16,14 +16,28 @@ db.once('open', () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/campgrounds', async (req, res) => {
 	const campgrounds = await Campground.find({});
 	res.render('campgrounds/index', { campgrounds });
 });
 
+// Order matter.
+app.get('/campgrounds/new', (req, res) => {
+	res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+	// res.send(req.body.campground);
+	const campground = new Campground(req.body.campground);
+	await campground.save();
+	res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
-	const { id } = req.params;
-	const camp = await Campground.findById(id);
+	// const { id } = req.params;
+	const camp = await Campground.findById(req.params.id);
 	res.render('campgrounds/show', { camp });
 });
 
