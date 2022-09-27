44935b96d138020435493662caf09a689468f816
mlthada7 <m.multahada@gmail.com>
2022-09-27 20:18:41 +0700

    serving static assets

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public'))); // To serve static assets in public folder
 
 //* <=== CAMPGROUND ROUTES ===>
 app.use('/campgrounds', campgroundsRoutes);
