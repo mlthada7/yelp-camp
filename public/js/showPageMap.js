c707a9fc4454c78ba5c86389440e5af1a09f8770
mlthada7 <m.multahada@gmail.com>
2022-10-13 18:00:51 +0700

    displaying a map and center the map on a campground

---
+++ public/js/showPageMap.js
@@ -0,0 +1,13 @@
+// TO MAKE THE MAP APPEAR
+// token and center value from script in show.ejs
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+	container: 'map', // container ID
+	style: 'mapbox://styles/mapbox/streets-v11', // style URL
+	center: campground.geometry.coordinates, // starting position [lng, lat]
+	zoom: 16, // starting zoom
+	projection: 'globe', // display the map as a 3D globe
+});
+
+// Create a default Marker and add it to the map.
+const marker1 = new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
