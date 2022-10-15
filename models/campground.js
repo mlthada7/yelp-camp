d737d5e8ab42710e663f2f96e06a4ae50d3bd70e
mlthada7 <m.multahada@gmail.com>
2022-10-15 22:42:04 +0700

    adding custom popups

--- models/campground.js
+++ models/campground.js
@@ -12,35 +12,46 @@ ImageSchema.virtual('thumbnail').get(function () {
 	return this.url.replace('/upload', '/upload/w_200');
 });
 
-const CampgroundSchema = new Schema({
-	title: String,
-	price: Number,
-	description: String,
-	location: String,
-	images: [ImageSchema],
-	geometry: {
-		// Following the geoJSON pattern
-		type: {
-			type: String,
-			enum: ['Point'],
-			required: true,
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+	{
+		title: String,
+		price: Number,
+		description: String,
+		location: String,
+		images: [ImageSchema],
+		geometry: {
+			// Following the geoJSON pattern
+			type: {
+				type: String,
+				enum: ['Point'],
+				required: true,
+			},
+			coordinates: {
+				type: [Number],
+				required: true,
+			},
 		},
-		coordinates: {
-			type: [Number],
-			required: true,
-		},
-	},
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-	},
-	reviews: [
-		{
-			// ref = Model
+		author: {
 			type: Schema.Types.ObjectId,
-			ref: 'Review',
+			ref: 'User',
 		},
-	],
+		reviews: [
+			{
+				// ref = Model
+				type: Schema.Types.ObjectId,
+				ref: 'Review',
+			},
+		],
+	},
+	opts
+);
+
+// mapbox expected 'properties' in order to customizing popup marker
+CampgroundSchema.virtual('properties.popupMarkup').get(function () {
+	return `<a href="/campgrounds/${this._id}">${this.title}</a>
+			<p>${this.description.substring(0, 20)}...</p>`;
 });
 
 // Mongoose Query Middleware
