a676670f793e7fb20ed2a93909240ca4bf56627d
mlthada7 <m.multahada@gmail.com>
2022-10-16 13:42:15 +0700

    handling mongo injection and sanitizing html with Joi

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const campgroundsRoutes = require('./routes/campgrounds');
 const reviewsRoutes = require('./routes/reviews');
@@ -37,6 +38,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public'))); // To serve static assets in public folder
+app.use(mongoSanitize());
 
 const sessionConfig = {
 	secret: 'mysecret',
